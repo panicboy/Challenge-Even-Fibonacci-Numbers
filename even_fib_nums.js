2ba45d6fa4d5cf70a150864e700f32c732a122be
panicboy <asasaki@gmail.com>
2016-06-13 11:12:33 -1000

    basic fibonacci function working

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -25,6 +25,16 @@ function _highestFibonacciNumber (maxFibValue){
   return highest;
 };
 
+function getFibs(maxNum){
+  var fibs = [1];
+  var currentNum = 1;
+  while  (fibs[fibs.length] >= maxNum) {
+    currentNum += fibs[fibs.length - 1];
+    fibs.push(currentNum);
+  }
+  return fibs;
+}
+
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
