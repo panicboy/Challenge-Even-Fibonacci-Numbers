5e8a6350241a404c26138afb5fd08706ad089790
panicboy <asasaki@gmail.com>
2016-06-13 11:40:09 -1000

    basic tests passed

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -9,7 +9,12 @@ function _sumFibs( maxFibValue ) {
   var sum = 0;
 
   // do your work here
-
+  theArray = getFibs(maxFibValue)
+  for (var i = theArray.length - 1; i >= 0; i--) {
+    if(theArray[i] % 2 === 0) {
+      sum += theArray[i];
+    }
+  }
   return sum;
 }
 
@@ -18,23 +23,25 @@ function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
 
   //define your base case, validate your input
-
+  var fibArray = getFibs(maxFibValue);
 
   //do your work here
-
+  var highest = fibArray[fibArray.length - 1];
   return highest;
 };
 
 function getFibs(maxNum){
-  var fibs = [1];
-  var currentNum = 1;
-  while  (fibs[fibs.length] >= maxNum) {
-    currentNum += fibs[fibs.length - 1];
-    fibs.push(currentNum);
+  var fibs = [1,2];
+  while  (fibs[fibs.length -1] < maxNum) {
+    currentNum = (fibs[fibs.length -1] + fibs[fibs.length -2]);
+    if(currentNum <= maxNum) {
+      fibs.push(currentNum);
+    } else {
+      return fibs;
+    }
   }
   return fibs;
 }
-
 /**
  * Do not modify code below.
  * You must be at least level 10 to understand.
